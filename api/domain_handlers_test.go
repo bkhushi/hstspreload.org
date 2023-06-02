68464940cee542576e556e58ec166ccaefcbfd52
Khushi Bhatamrekar <khushib@google.com>
2023-06-02 23:29:08 +0000

    preloaded tld's should not be preloaded again test. not working

--- api/domain_handlers_test.go
+++ api/domain_handlers_test.go
@@ -347,3 +347,40 @@ func TestDeletionAndStatusChange(t *testing.T) {
 		t.Errorf("Status changed")
 	}
 }
+type preloadedTLDTest struct {
+	domain   string
+	expected hstspreload.Issues
+	method   string
+	url      string
+}
+
+func TestTLD(t *testing.T) {
+	tldTestSequence := []preloadedTLDTest{
+		{"example.app",
+			hstspreload.Issues{Errors: []hstspreload.Issue{{Code: "domain.preloaded_tld"}}},
+			"POST", "?domain=example.app",
+		},
+		{"example.dev",
+			hstspreload.Issues{Errors: []hstspreload.Issue{{Code: "domain.preloaded_tld"}}},
+			"POST", "?domain=example.dev",
+		},
+	}
+	for _, tt := range tldTestSequence {
+		w := httptest.NewRecorder()
+		w.Body = &bytes.Buffer{}
+
+		_, err := http.NewRequest(tt.method, tt.url, nil)
+		if err != nil {
+			t.Fatalf("[%s] %s", tt.domain, err)
+		}
+
+		var iss hstspreload.Issues
+		if err := json.Unmarshal(w.Body.Bytes(), &iss); err != nil {
+			t.Fatalf("[%s] %s", tt.domain, err)
+		}
+		if !iss.Match(*&tt.expected) {
+			t.Errorf("[%s] Issues do not match wanted: %#v", tt.domain, iss)
+		}
+
+	}
+}
